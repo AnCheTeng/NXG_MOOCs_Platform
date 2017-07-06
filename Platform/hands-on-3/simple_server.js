de542643a4502ead93c8f01ab591e6a5c265db39
AnCheTeng <angeldan0627@gmail.com>
2017-07-06 13:15:54 +0800

    Add basic templates

--- Platform/hands-on-3/simple_server.js
+++ Platform/hands-on-3/simple_server.js
@@ -1,10 +1,7 @@
-var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var express = require('express');
 
 var app = express();
-var Packet = require('./model/Packet');
-mongoose.connect('mongodb://localhost/iotplatform');
 
 app.get('/', function(request, response) {
   response.send("Hello World!");
